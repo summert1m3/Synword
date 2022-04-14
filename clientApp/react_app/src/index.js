e3e0e3f02e7666f301b1e98b54c38df752afce6c
rea1stuff <40441464+rea1stuff@users.noreply.github.com>
2022-04-14 17:44:09 +0300

    fix: change reactDOM.render to createRoot

--- clientApp/react_app/src/index.js
+++ clientApp/react_app/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App'
+import { createRoot } from "react-dom/client";
 import "./styles/app.css"
 import "./components/header/header.css"
 import "./components/main/main.css"
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import "./components/main/Textarea/textarea.css"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+createRoot(document.getElementById("root")).render(
+    <App />
+);
