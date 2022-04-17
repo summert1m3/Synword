bb5091bb6d0975cea9d180d6a4c8c4a6e66b4414
rea1stuff <40441464+rea1stuff@users.noreply.github.com>
2022-04-17 13:20:03 +0300

    client/feat: add adaptability to links

--- clientApp/react_app/src/components/main/ResultsScreen/Link/Link.js
+++ clientApp/react_app/src/components/main/ResultsScreen/Link/Link.js
@@ -1,15 +1,55 @@
 import React from "react";
 
 class Link extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { windowWidth: window.innerWidth };
+    }
+
+    handleResize = (e) => {
+        this.setState({ windowWidth: window.innerWidth });
+    };
+
+    componentDidMount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    componentWillUnmount() {
+        window.addEventListener("resize", this.handleResize);
+    }
+
+    sliceLink(link, limit) {
+        let sliced = link.slice(0, limit);
+
+        if (sliced.length < link.length) {
+            sliced += '...';
+        }
+
+        return sliced;
+    }
+
     render() {
         const {
             link,
             percent
         } = this.props;
 
+        let width = this.state.windowWidth;
+        let sliced;
+
+        if (width > 1044) {
+            sliced = this.sliceLink(link, 60);
+        }
+        else if (width > 715) {
+            sliced = this.sliceLink(link, 35);
+        }
+        else {
+            sliced = this.sliceLink(link, 25);
+        }
+
         return (
             <div className="links__plagiarism-body">
-                <a href={link}>{link}</a>
+                <a href={link}>{sliced}</a>
                 <p className="percent__links">{percent}%</p>
             </div>
         );
