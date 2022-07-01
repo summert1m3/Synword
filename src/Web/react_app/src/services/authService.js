4078517a1b6e8796060040f609cd6c7d822f095b
rea1stuff <40441464+rea1stuff@users.noreply.github.com>
2022-07-01 18:37:14 +0300

    client: verify jwt expiration date

--- src/Web/react_app/src/services/authService.js
+++ src/Web/react_app/src/services/authService.js
@@ -1,22 +1,37 @@
 import ApiBaseService from "./apiBaseService";
 import Cookies from 'js-cookie';
+import jwt_decode from "jwt-decode";
 
 class AuthService {
-    
-    async guestRegisterRequest(signal) {
+    async authorizedPostRequest(url, form, signal) {
+        await this.verifyJWT(signal);
+
+        const jwt = Cookies.get('jwt');
+
         let headers = {
             "Accept": "application/json",
-            "Content-Type": "multipart/form-data",
+            "Authorization": `Bearer ${jwt}`
         };
 
         let data = await ApiBaseService.postRequest(
-            ApiBaseService.guestRegisterUrl, 
+            url,
             headers,
-            null,
+            form,
             signal
-            );
+        );
+
+        return data;
+    }
+
+    async verifyJWT(signal) {
+        const jwt = Cookies.get('jwt');
+        const decodedJwt = jwt_decode(token);
         
-        Cookies.set('userId', data.userId);
+        var current_time = Date.now() / 1000;
+
+        if (!jwt || decodedJwt.exp < current_time) {
+            await this.guestAuthenticateRequest(signal);
+        }
     }
 
     async guestAuthenticateRequest(signal) {
@@ -32,49 +47,37 @@ class AuthService {
         form.append("userId", userId);
 
         let data = await ApiBaseService.postRequest(
-            ApiBaseService.guestAuthenticateUrl, 
+            ApiBaseService.guestAuthenticateUrl,
             headers,
             form,
             signal
-            );
+        );
 
         Cookies.set('jwt', data.token);
     }
 
-    async authorizedPostRequest(url, form, signal) {
-        await this.verifyJWT(signal);
-
-        const jwt = Cookies.get('jwt');
-
-        let headers = {
-            "Accept": "application/json",
-            "Authorization": `Bearer ${jwt}`
-        };
-
-        let data = await ApiBaseService.postRequest(
-            url, 
-            headers,
-            form,
-            signal
-            );
-
-        return data;
-    }
-
     async verifyUserId(signal) {
         const userId = Cookies.get('userId');
-        
-        if(!userId) {
+
+        if (!userId) {
             await this.guestRegisterRequest(signal);
         }
     }
 
-    async verifyJWT(signal) {
-        const jwt = Cookies.get('jwt');
+    async guestRegisterRequest(signal) {
+        let headers = {
+            "Accept": "application/json",
+            "Content-Type": "multipart/form-data",
+        };
 
-        if(!jwt) {
-            await this.guestAuthenticateRequest(signal);
-        }
+        let data = await ApiBaseService.postRequest(
+            ApiBaseService.guestRegisterUrl,
+            headers,
+            null,
+            signal
+        );
+
+        Cookies.set('userId', data.userId);
     }
 }
 
