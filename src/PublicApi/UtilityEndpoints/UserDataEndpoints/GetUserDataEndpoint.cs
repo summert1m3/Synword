cd842db23c88c7eb35198c7f52340d4cba4d5953
rea1stuff <40441464+rea1stuff@users.noreply.github.com>
2022-07-09 15:21:50 +0300

    feat: add user data endpoint

---
+++ src/PublicApi/UtilityEndpoints/UserDataEndpoints/GetUserDataEndpoint.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Claims;
+using Ardalis.ApiEndpoints;
+using Ardalis.GuardClauses;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using Synword.Domain.Entities.UserAggregate;
+using Synword.Domain.Interfaces.Repository;
+using Synword.Infrastructure.Identity;
+
+namespace Synword.PublicApi.UtilityEndpoints.UserDataEndpoints;
+
+public class GetUserDataEndpoint : EndpointBaseAsync
+    .WithoutRequest
+    .WithActionResult
+{
+    private readonly ISynwordRepository<User> _userRepository;
+    private readonly UserManager<AppUser>? _userManager;
+    
+    public GetUserDataEndpoint(
+        ISynwordRepository<User> userRepository,
+        UserManager<AppUser>? userManager)
+    {
+        _userRepository = userRepository;
+        _userManager = userManager;
+    }
+    
+    [HttpGet("getUserData")]
+    [Authorize]
+    [SwaggerOperation(
+        Tags = new[] { "Utility" }
+    )]
+    public override async Task<ActionResult> HandleAsync(
+        CancellationToken cancellationToken = default)
+    {
+        string? uId = User.FindFirst(ClaimTypes.NameIdentifier)!.Value;
+
+        AppUser userIdentity = await _userManager!.FindByIdAsync(uId);
+        Guard.Against.Null(userIdentity);
+        var roles = await _userManager.GetRolesAsync(userIdentity);
+
+        User? user = await _userRepository.GetByIdAsync(uId);
+        Guard.Against.Null(user);
+
+        return Ok(
+            new GetUserDataResponse(roles.First(), user.Coins.Value));
+    }
+}
---
+++ src/PublicApi/UtilityEndpoints/UserDataEndpoints/GetUserDataResponse.cs
@@ -0,0 +1,13 @@
+﻿namespace Synword.PublicApi.UtilityEndpoints.UserDataEndpoints;
+
+public class GetUserDataResponse
+{
+    public GetUserDataResponse(string role, int balance)
+    {
+        Role = role;
+        Balance = balance;
+    }
+    
+    public string Role { get; }
+    public int Balance { get; }
+}
